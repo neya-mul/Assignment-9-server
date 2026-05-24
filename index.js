f331c48cbd54084787eda129fc500884ad019d4f
neya-mul <neyamulislam946@gmail.com>
2026-05-24 14:12:33 +0600

    bug fixed

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let adoptionCollection;
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -65,7 +65,15 @@ async function run() {
             res.json(result);
         })
 
-        app.get('/pets/:id', async (req, res) => {
+        app.get('/pets/:id', (req, res, next) => {
+            const header = req.headers.authorization
+            res.set('Cache-Control', 'no-store');
+
+            console.log(header);
+            next()
+
+
+        }, async (req, res) => {
             const id = req.params.id;
 
             // Check if it's a valid 24-character hex code before trying to use new ObjectId
@@ -119,9 +127,9 @@ async function run() {
         app.post('/adoption-requests', async (req, res) => {
 
             const { petId, adopterId } = req.body
-            const existing = await adoptionCollection.findOne({petId, adopterId})
-            if(existing){
-                return res.status(409).json({alreadyRequested: true, message: 'You have already sent an adoption request for this pet.'})
+            const existing = await adoptionCollection.findOne({ petId, adopterId })
+            if (existing) {
+                return res.status(409).json({ alreadyRequested: true, message: 'You have already sent an adoption request for this pet.' })
             }
 
             const result = await adoptionCollection.insertOne(req.body)
